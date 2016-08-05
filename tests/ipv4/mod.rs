e07db84aa038ebdf1d699ffc0ee78c962bcee9ad
Linus Färnstrand <faern@faern.net>
2016-08-05 07:45:00 +0200

    ipv4 recv test works

--- tests/ipv4/mod.rs
+++ tests/ipv4/mod.rs
@@ -11,7 +11,8 @@ use pnet::packet::ethernet::{EtherTypes, EthernetPacket, MutableEthernetPacket};
 use pnet::packet::ipv4::{Ipv4Packet, MutableIpv4Packet};
 use pnet::packet::{MutablePacket, Packet};
 
-use rips::ipv4::{Ipv4Tx, Ipv4Listener};
+use rips::ipv4::{Ipv4Tx, Ipv4Rx, Ipv4Listener};
+use rips::ethernet::{EthernetListener, EthernetRx};
 
 pub struct MockIpv4Listener {
     pub tx: mpsc::Sender<Vec<u8>>,
@@ -55,45 +56,47 @@ fn simple_send() {
     assert_eq!(ip_pkg.payload(), [101, 204]);
 }
 
-// #[test]
-// fn test_simple_recv() {
-//     let source_ip = Ipv4Addr::new(10, 1, 2, 3);
-//     let target_ip = Ipv4Addr::new(10, 1, 2, 2);
-//
-//     let (tx, rx) = mpsc::channel();
-//     let ipv4_listener = MockIpv4Listener { tx: tx };
-//     let mut ipv4_ip_listeners = HashMap::new();
-//     ipv4_ip_listeners.insert(IpNextHeaderProtocols::Igmp,
-//                              Box::new(ipv4_listener) as Box<Ipv4Listener>);
-//
-//     let mut ipv4_listeners = HashMap::new();
-//     ipv4_listeners.insert(target_ip, ipv4_ip_listeners);
-//
-//     let (_ethernet, _, inject_handle, _) = ::dummy_ipv4(Arc::new(Mutex::new(ipv4_listeners)));
-//
-//     let size = EthernetPacket::minimum_packet_size() + Ipv4Packet::minimum_packet_size() + 2;
-//     let mut buffer = vec![0; size];
-//     {
-//         let mut eth_pkg = MutableEthernetPacket::new(&mut buffer[..]).unwrap();
-//         eth_pkg.set_ethertype(EtherTypes::Ipv4);
-//         let mut ip_pkg = MutableIpv4Packet::new(eth_pkg.payload_mut()).unwrap();
-//         ip_pkg.set_header_length(5); // 5 is for no option fields
-//         ip_pkg.set_source(source_ip);
-//         ip_pkg.set_destination(target_ip);
-//         ip_pkg.set_next_level_protocol(IpNextHeaderProtocols::Igmp);
-//         ip_pkg.set_payload(&[67, 99]);
-//     }
-//
-//     inject_handle.send(Ok(buffer.into_boxed_slice())).unwrap();
-//
-//     let pkg = match rx.recv() {
-//         Ok(p) => p,
-//         Err(e) => panic!("NOOO: {}", e),
-//     };
-//     let ip_pkg = Ipv4Packet::new(&pkg[..]).unwrap();
-//     assert_eq!(ip_pkg.get_source(), source_ip);
-//     assert_eq!(ip_pkg.get_destination(), target_ip);
-//     assert_eq!(ip_pkg.get_next_level_protocol(),
-//                IpNextHeaderProtocols::Igmp);
-//     assert_eq!(ip_pkg.payload(), [67, 99]);
-// }
+#[test]
+fn custom_igmp_recv() {
+    let source_ip = Ipv4Addr::new(10, 1, 2, 3);
+    let target_ip = Ipv4Addr::new(10, 1, 2, 2);
+
+    let (tx, rx) = mpsc::channel();
+    let ipv4_listener = MockIpv4Listener { tx: tx };
+    let mut ipv4_ip_listeners = HashMap::new();
+    ipv4_ip_listeners.insert(IpNextHeaderProtocols::Igmp,
+                             Box::new(ipv4_listener) as Box<Ipv4Listener>);
+
+    let mut ipv4_listeners = HashMap::new();
+    ipv4_listeners.insert(target_ip, ipv4_ip_listeners);
+
+    let (mut channel, interface, inject_handle, _) = ::dummy_ethernet(0);
+    let ipv4_rx = Ipv4Rx::new(Arc::new(Mutex::new(ipv4_listeners)));
+    let ethernet_rx = EthernetRx::new(vec![ipv4_rx]).spawn(channel.1);
+
+    let size = EthernetPacket::minimum_packet_size() + Ipv4Packet::minimum_packet_size() + 2;
+    let mut buffer = vec![0; size];
+    {
+        let mut eth_pkg = MutableEthernetPacket::new(&mut buffer[..]).unwrap();
+        eth_pkg.set_ethertype(EtherTypes::Ipv4);
+        let mut ip_pkg = MutableIpv4Packet::new(eth_pkg.payload_mut()).unwrap();
+        ip_pkg.set_header_length(5); // 5 is for no option fields
+        ip_pkg.set_source(source_ip);
+        ip_pkg.set_destination(target_ip);
+        ip_pkg.set_next_level_protocol(IpNextHeaderProtocols::Igmp);
+        ip_pkg.set_payload(&[67, 99]);
+    }
+
+    inject_handle.send(Ok(buffer.into_boxed_slice())).unwrap();
+
+    let pkg = match rx.recv() {
+        Ok(p) => p,
+        Err(e) => panic!("NOOO: {}", e),
+    };
+    let ip_pkg = Ipv4Packet::new(&pkg[..]).unwrap();
+    assert_eq!(ip_pkg.get_source(), source_ip);
+    assert_eq!(ip_pkg.get_destination(), target_ip);
+    assert_eq!(ip_pkg.get_next_level_protocol(),
+               IpNextHeaderProtocols::Igmp);
+    assert_eq!(ip_pkg.payload(), [67, 99]);
+}
