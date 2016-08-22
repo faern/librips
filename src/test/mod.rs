fe7d7b1e277a1fc54e6ce3781d3867aafd39d957
Linus Färnstrand <faern@faern.net>
2016-08-22 22:53:43 +0200

    Add failing test on no --features

--- src/test/mod.rs
+++ src/test/mod.rs
@@ -1,3 +1,16 @@
 pub mod ethernet;
 
 pub mod ipv4;
+
+#[cfg(all(not(feature = "unit-tests"), not(feature = "integration-tests")))]
+#[test]
+fn tests_should_be_run_with_features() {
+    let msg = "\n
+        ===========================\n
+        To run unit tests:\n
+        \tcargo test --features \"unit-tests\".\n
+        Integration tests:\n
+        \tcargo test --features \"integration-tests\"\n
+        ===========================\n";
+    panic!(msg);
+}
--- tests/lib.rs
+++ tests/lib.rs
@@ -2,24 +2,24 @@ extern crate pnet;
 extern crate ipnetwork;
 extern crate rips;
 
-#[cfg(all(test, not(feature = "unit-tests")))]
+#[cfg(all(test, feature = "integration-tests"))]
 mod ethernet;
 
 // mod stack;
 
-#[cfg(all(test, not(feature = "unit-tests")))]
+#[cfg(all(test, feature = "integration-tests"))]
 mod arp;
 
-#[cfg(all(test, not(feature = "unit-tests")))]
+#[cfg(all(test, feature = "integration-tests"))]
 mod ipv4;
 
-#[cfg(all(test, not(feature = "unit-tests")))]
+#[cfg(all(test, feature = "integration-tests"))]
 mod icmp;
 
-#[cfg(all(test, not(feature = "unit-tests")))]
+#[cfg(all(test, feature = "integration-tests"))]
 mod udp;
 
-#[cfg(all(test, not(feature = "unit-tests")))]
+#[cfg(all(test, feature = "integration-tests"))]
 mod helper {
     use std::sync::mpsc::{Receiver, Sender};
     use std::io;
